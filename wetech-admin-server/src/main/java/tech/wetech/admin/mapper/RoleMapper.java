fbba4766e027262318a44a583915b80be93948e1
shuirouyan <1141761757@qq.com>
2021-05-05 11:09:49 +0800

    set up upstream prepare

--- wetech-admin-server/src/main/java/tech/wetech/admin/mapper/RoleMapper.java
+++ wetech-admin-server/src/main/java/tech/wetech/admin/mapper/RoleMapper.java
@@ -3,6 +3,9 @@ package tech.wetech.admin.mapper;
 import tech.wetech.admin.model.entity.Role;
 import tech.wetech.mybatis.mapper.BaseMapper;
 
+/**
+ * @author kangchen
+ */
 public interface RoleMapper extends BaseMapper<Role> {
 
 }
